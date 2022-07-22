d3090da418311f66588d844914670ebd609cb18c
Xhanticode <xhantimpu@gmail.com>
2022-07-22 12:28:05 +0200

    Friday 12:27

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+}
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    projects: [
+      {
+        img: "https://i.postimg.cc/65Dc5M6F/Restaurantly-Site.png",
+      },
+      {
+        img: "https://i.postimg.cc/qMk0DGdV/Mock-Portfolio-Site.png",
+      },
+      {
+        img: "https://i.postimg.cc/T1Ypqvdh/iX-Site.png",
+      },
+      {
+        img: "https://i.postimg.cc/CxVpWH8s/Daily-Planner-Site.png",
+      },
+    ],
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {},
+});
