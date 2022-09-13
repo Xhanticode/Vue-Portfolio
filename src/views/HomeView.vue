7571b75d845855e43e7e3c65be8d6a0e1306dd05
Xhanti <xhantimpu@gmail.com>
2022-09-13 16:13:45 +0200

    Resume

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -495,8 +495,8 @@
           <span class="section-underline"></span>
           <p>
             Click
-            <a id="cv-link" href="./PDF's/Xabiso Xhanti Mpu CV.pdf">here</a> to
-            dowloaded my resumé.
+            <a :href="linkedin" id="cv-link"  >here</a> to
+            view my LinkedIn profile and resumé.
           </p>
           <!-- timeline -->
         </div>
@@ -657,7 +657,7 @@
           <h3 class="section-title">Portfolio</h3>
           <h2 class="section-subtitle">Selected Work</h2>
           <span class="section-underline"></span>
-          <p>Bet you wanna see some the stuff I am boasting about?</p>
+          <p>Bet you wanna see some of the stuff I am boasting about?</p>
         </div>
         <div class="section-content">
           <!-- Restaurantly Modal  -->
@@ -850,8 +850,8 @@
             margin-inline: auto;
           "
         >
-          <p>&copy Xhanti</p>
-          <p>copyright 2022</p>
+          <p>by Xhanti Mpu</p>
+          <p>&copy; copyright 2022</p>
         </div>
       </section>
     </div>
@@ -961,6 +961,11 @@
 // import { createReadStream } from "fs";
 
 export default {
+  data() {
+        return {
+            linkedin: 'http://linkedin.com/in/xhantimpu'
+        }
+    },
   computed: {
     getprojects() {
       return this.$store.state.projects;
@@ -972,7 +977,63 @@ export default {
       observer.observe(entry);
     });
   },
-  methods() {},
+  methods() {
+    /**
+ * Initially the elements are positioned in front of the camera.
+ * We define here the transformations to position them in the scene.
+ * When clicking on an item, we apply the inversed transformation but on the main container to "zoom" on the item
+ */
+const articles = new Map([
+  ["6392322", { tx: "-90%", tz: "-70vmin", ry: "60deg" }],
+  ["1761279", { tz: "-110vmin" }],
+  ["1679772", { tx: "90%", tz: "-70vmin", ry: "-60deg" }]
+]);
+
+window.addEventListener("load", () => {
+  const main = document.querySelector("main");
+
+  for (const [id, { tx, tz, ry }] of articles.entries()) {
+    main.appendChild(makeArticleElement(id, tx, tz, ry));
+  }
+
+  document.addEventListener("click", ({ target }) => {
+    const targetId = target.closest("article")?.id;
+    let [itx, itz, iry] = [0, 0, 0]; // inversed transformation to apply to the main element
+
+    if (targetId && main.dataset.focus !== targetId) {
+      // zoom in
+      const { tx, tz, ry } = articles.get(targetId) || {};
+      [itx, itz, iry] = [tx, tz, ry].map(inverseTransformation);
+      main.setAttribute("data-focus", targetId);
+    } else {
+      // zoom out
+      main.removeAttribute("data-focus");
+    }
+    main.style.transform = `rotateY(${iry}) translate3d(${itx}, 0, ${itz})`;
+  });
+});
+
+// e.g. turn "90%" into "-90%" or "-10vmin" into "10vmin"
+function inverseTransformation(transform) {
+  if (!transform) return 0;
+  const [_, value, unit] = transform.match(/(-?\d+)(.*)/);
+  return `${-Number(value)}${unit}`;
+}
+
+function makeArticleElement(id, tx = 0, tz = 0, ry = 0) {
+  const img = document.createElement("img");
+  img.src = `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500`;
+
+  const button = document.createElement("button");
+  button.appendChild(img);
+
+  const element = document.createElement("article");
+  element.id = id;
+  element.style.transform = `translate3d(${tx}, 0, ${tz}) rotateY(${ry})`;
+  element.appendChild(button);
+  return element;
+}
+  },
 };
 const observer = new IntersectionObserver(
   (entries) => {
